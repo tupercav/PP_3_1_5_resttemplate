76b1892276d724c57bc8afc34cf6c40645e19a41
tupercav <tupercav@gmail.com>
2022-07-26 18:05:42 +0300

    1st commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import org.example.model.User;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+        Communication communication = context.getBean("communication", Communication.class);
+        String result = "";
+        List<String> cookies = communication.getCookie();
+
+        //Saving newUser to get 1st part of result
+        User newUser = new User(3L, "James", "Brown", (byte) 33);
+        result += communication.saveUser(newUser, cookies);
+
+        //Edititing newUser and saving to get 2nd part of result
+        newUser.setName("Thomas");
+        newUser.setLastName("Shelby");
+        result += communication.saveUser(newUser, cookies);
+
+        //Deleting newUser to get 3rd part of result
+        result += communication.deleteUser(3, cookies);
+
+        System.out.println("Result is " + result);
+        System.out.println("Qty of symbols in result: " + result.length());
+
+    }
+}
---
+++ src/main/java/org/example/Communication.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import org.example.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.awt.image.RescaleOp;
+import java.util.List;
+import java.util.SortedMap;
+import java.util.stream.Collectors;
+
+@Component
+public class Communication {
+    private final String URL = "http://94.198.50.185:7081/api/users";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public List<String> getCookie() {
+        ResponseEntity<List> responseEntity = restTemplate.getForEntity(URL, List.class);
+        List<String> cookie = responseEntity.getHeaders().get("Set-Cookie");
+        return cookie;
+    }
+
+    public HttpHeaders setHttpHeaders(List<String> cookies) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("Cookie", cookies.stream().collect(Collectors.joining(";")));
+        return httpHeaders;
+    }
+
+    public String saveUser (User user, List<String> cookies) {
+        HttpHeaders headers = setHttpHeaders(cookies);
+        HttpEntity<User> requestBody = new HttpEntity<>(user, headers);
+        ResponseEntity<String> responseEntity = restTemplate.postForEntity(URL, requestBody, String.class);
+        return responseEntity.getBody();
+    }
+
+    public String deleteUser (int id, List<String> cookies) {
+        HttpHeaders headers = setHttpHeaders(cookies);
+        HttpEntity<User> requestBody = new HttpEntity<>(headers);
+        ResponseEntity<String> responseEntity = restTemplate.exchange(URL+"/"+id, HttpMethod.DELETE, requestBody, String.class);
+        return responseEntity.getBody();
+    }
+}
